a72c3826792e01170d2c8c65a7ad606d2c6bd648
Steven Murray <steven.g.murray@asu.edu>
2020-08-07 13:00:25 -0700

    remove cached templates for now

--- TheHaloMod/settings/production.py
+++ TheHaloMod/settings/production.py
@@ -61,15 +61,15 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
-TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
-    (
-        "django.template.loaders.cached.Loader",
-        [
-            "django.template.loaders.filesystem.Loader",
-            "django.template.loaders.app_directories.Loader",
-        ],
-    )
-]
+# TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
+#     (
+#         "django.template.loaders.cached.Loader",
+#         [
+#             "django.template.loaders.filesystem.Loader",
+#             "django.template.loaders.app_directories.Loader",
+#         ],
+#     )
+# ]
 
 # EMAIL
 # ------------------------------------------------------------------------------
